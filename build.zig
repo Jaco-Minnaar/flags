4821996a7ba4ad0cd1443d1d55e72e91b9854575
Jaco Minnaar <jminnaar@bscglobal.com>
2025-08-21 15:17:02 +0200

    update build.zig to zig 0.15.1

--- build.zig
+++ build.zig
@@ -15,8 +15,7 @@ pub fn build(b: *std.Build) void {
     const tests_step = b.step("test", "Run tests");
 
     const tests = b.addTest(.{
-        .root_source_file = root,
-        .target = target,
+        .root_module = mod,
     });
 
     const tests_run = b.addRunArtifact(tests);
@@ -34,13 +33,16 @@ pub fn build(b: *std.Build) void {
         "Example to run for example step (default = overview)",
     ) orelse .overview;
 
-    const example = b.addExecutable(.{
-        .name = "example",
+    const example_mod = b.createModule(.{
         .root_source_file = b.path(b.fmt("examples/{s}.zig", .{@tagName(example_option)})),
         .target = target,
         .optimize = optimize,
     });
-    example.root_module.addImport("flags", mod);
+    example_mod.addImport("flags", mod);
+    const example = b.addExecutable(.{
+        .name = "example",
+        .root_module = example_mod,
+    });
     const run_example = b.addRunArtifact(example);
     if (b.args) |args| run_example.addArgs(args);
     example_step.dependOn(&run_example.step);
